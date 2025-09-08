2af8f743457f15e0d580bc0c0ceb08052822a389
Aditi Trivedi <adititrivedi2017@gmail.com>
2025-09-08 14:47:01 +0530

    feat: App routes (paths)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,38 @@
-import React, { useEffect } from "react";
-import "./App.css";
-import { Log } from "./lib/logger";
+import React from "react";
+import { Routes, Route, Link, useLocation } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button, Container } from "@mui/material";
+import ShortenPage from "./routes/ShortenPage";
+import StatsPage from "./routes/StatsPage";
+import RedirectPage from "./routes/RedirectPage";
 
-function App() {
-  useEffect(() => {
-    (async () => {
-      try {
-        await Log("frontend", "info", "component", "app started");
-      } catch (e) {
-        //
-      }
-    })();
-  }, []);
-
-  return <div>URL Shortener</div>;
+export default function App() {
+  const location = useLocation();
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            URL Shortener
+          </Typography>
+          {location.pathname !== "/" && (
+            <Button color="inherit" component={Link} to="/">
+              Shorten
+            </Button>
+          )}
+          {location.pathname !== "/stats" && (
+            <Button color="inherit" component={Link} to="/stats">
+              Stats
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
+      <Container sx={{ mt: 3, mb: 6 }}>
+        <Routes>
+          <Route path="/" element={<ShortenPage />} />
+          <Route path="/stats" element={<StatsPage />} />
+          <Route path=":code" element={<RedirectPage />} />
+        </Routes>
+      </Container>
+    </>
+  );
 }
-
-export default App;
